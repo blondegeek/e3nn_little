f0006a37a469f8c22acff0b94654806f16748b17
Mario Geiger <geiger.mario@gmail.com>
2020-11-27 09:48:37 +0100

    swish and 1/2 o^2

--- e3nn_little/nn/models/baseline.py
+++ e3nn_little/nn/models/baseline.py
@@ -107,7 +107,7 @@ class Network(torch.nn.Module):
 
         # even + odd^2 = even
         assert h.shape[1] == 2
-        h = h[:, 0] + h[:, 1].pow(2)
+        h = h[:, 0] + h[:, 1].pow(2).mul(0.5)
         h = h.view(-1, 1)
 
         if self.mean is not None and self.std is not None:
